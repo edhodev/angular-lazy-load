9f5f1f02b6f6d4da2d622d79a0ff59c86cca6406
edhodev <riidho.lfc@gmail.com>
2021-01-05 17:59:34 +0800

    pagination

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -10,23 +10,37 @@ export class UserComponent implements OnInit {
   data: any = []
   total: number = 0
   limit: number = 10
+  page: number = 0
+  listPage: any = []
+  totalPage: number = 0
   constructor(
     private http: HttpRequestService
   ) { }
 
   ngOnInit(): void {
-      this.getData(this.limit)
+      this.getData(this.limit, this.page)
   }
 
-  getData(limit: number) {
-    this.http.get("user?limit=" + limit).then((response: any) => {
+  getData(limit: number, page: number) {
+    this.http.get("user?limit=" + limit + "&page=" + page).then((response: any) => {
+      this.listPage = []
       this.data = response.data
       this.total = response.total
+      this.totalPage = this.total / limit
+      this.page = response.page
+      for (let index = 0; index < this.totalPage; index++) {
+        this.listPage.push(index+1)
+      }
     })
   }
+  
   show()
   {
-    this.getData(this.limit)
+    this.getData(this.limit, 0)
+  }
+
+  changePage(item: number) {
+    this.getData(this.limit, item-1)
   }
 
 }
